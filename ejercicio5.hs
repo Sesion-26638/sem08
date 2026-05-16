ae860426838df68bc4bc6500f2dfc2ea5f0cb704
BanyelaneYT <127866310+BanyelaneYT@users.noreply.github.com>
2026-05-16 12:54:49 -0500

    correccion de errores

--- ejercicio5.hs
+++ ejercicio5.hs
@@ -1,9 +1,5 @@
-import Data.Char (toUpper)
-
 convertir :: [String]-> [(String,Int)]
-convertir x
-    | length x < 5 = map (\x -> (x, length x)) (filter (\x -> length x < 5) x)
-    | otherwise = error "Los elementos de la lista deben tener menos de 5 caracteres"
+convertir x = map (\x -> (x, length x)) (filter (\x -> length x < 5) x)
 
 main::IO()
 main=do
